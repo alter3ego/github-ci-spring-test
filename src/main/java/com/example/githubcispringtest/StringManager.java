99fe5ac92ef4bd1c6912cc915619f9e219c27740
Oleksandr Omelchenko <alexmir_98@ukr.net>
2022-12-19 22:25:35 +0200

    actual

--- src/main/java/com/example/githubcispringtest/StringManager.java
+++ src/main/java/com/example/githubcispringtest/StringManager.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class StringManager {
     public String getReverseUppercaseString(String string) {
-        StringBuilder stringBuilder = new StringBuilder(string.toLowerCase());
+        StringBuilder stringBuilder = new StringBuilder(string.toUpperCase());
 
         return stringBuilder.reverse().toString();
     }
