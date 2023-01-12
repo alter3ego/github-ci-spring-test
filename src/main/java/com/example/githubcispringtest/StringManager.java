794964f6578859bbb4c62be843605ff7d72579ec
Alter3Ego <alexmir_98@ukr.net>
2023-01-12 20:12:19 +0200

    wrong01

--- src/main/java/com/example/githubcispringtest/StringManager.java
+++ src/main/java/com/example/githubcispringtest/StringManager.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class StringManager {
     public String getReverseUppercaseString(String string) {
+        //wrong case
         StringBuilder stringBuilder = new StringBuilder(string.toLowerCase());
         return stringBuilder.reverse().toString();
     }
